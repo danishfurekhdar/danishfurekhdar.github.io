15d47c58b909cb2e07f23b0db2cd7203659d3495
danishfurkh <furkhdanish@gmail.com>
2025-04-11 13:22:37 +0200

    Upload/update full blog directory

--- load.bat
+++ load.bat
@@ -2,7 +2,7 @@
 cd /d "C:\Users\furkh\Downloads\danishfurekhdar.github.io-1eec030f2db77b4a625bc74a54481111921a4a84\danishfurekhdar.github.io-1eec030f2db77b4a625bc74a54481111921a4a84"
 
 REM Initialize git only if .git doesn't exist
-IF NOT EXIST ".git" (
+IF NOT EXIST ".git\config" (
     git init
     git remote add origin https://github.com/danishfurekhdar/danishfurekhdar.github.io
 ) ELSE (
@@ -17,7 +17,7 @@ git pull --rebase origin main
 
 REM Stage and commit changes
 git add .
-git commit -m "Upload/update full blog directory"
+git diff --cached --quiet || git commit -m "Upload/update full blog directory"
 
 REM Push to GitHub
 git push origin main
